961989ad9217584466e5b20a6dba426b885a5e0b
Davi Maximo Quoos <davimaximoquooss@gmail.com>
2025-11-05 00:26:32 -0300

    fix: concerto de responsividade

--- scripts.js
+++ scripts.js
@@ -1,7 +1,21 @@
 const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
+const navLinks = document.querySelector(".ul-nav");
 
 menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("open");
   navLinks.classList.toggle("active");
+
+  if (navLinks.classList.contains("active")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "";
+  }
+});
+
+navLinks.addEventListener("click", (e) => {
+  if (e.target === navLinks) {
+    navLinks.classList.remove("active");
+    menuToggle.classList.remove("open");
+    document.body.style.overflow = "";
+  }
 });
